27cda6a8db82c739acdf065d27a6b409969eaf45
avikuuuu <ku.kumar.avdesh786@gmail.com>
2023-06-11 22:56:24 +0530

    done delete funtion

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -35,10 +35,28 @@ class _TODOListPageState extends State<TODOListPage> {
             itemCount: items.length,
             itemBuilder: (context, index) {
               final item = items[index] as Map;
+              final id = item['_id'] as String;
               return ListTile(
                 leading: CircleAvatar(child: Text("${index + 1}")),
                 title: Text(item['title']),
                 subtitle: Text(item['description']),
+                trailing: PopupMenuButton(onSelected: (value) {
+                  if (value == 'edit') {
+                  } else if (value == 'delete') {
+                    deleteById(id);
+                  }
+                }, itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      child: Text('Edit'),
+                      value: 'edit',
+                    ),
+                    PopupMenuItem(
+                      child: Text('Delete'),
+                      value: 'delete',
+                    ),
+                  ];
+                }),
               );
             },
           ),
@@ -54,6 +72,22 @@ class _TODOListPageState extends State<TODOListPage> {
   void navigateToAddPage() {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => const AddTodoPage()));
+  }
+
+  Future<void> deleteById(String id) async {
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+
+    if (response.statusCode == 200) {
+      successMessage('deleted successfully');
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
+      showErrorMessage('failed to delete');
+    }
   }
 
   Future<void> fetchTODO() async {
@@ -72,5 +106,21 @@ class _TODOListPageState extends State<TODOListPage> {
     setState(() {
       isLoading = true;
     });
+  }
+
+  void successMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
