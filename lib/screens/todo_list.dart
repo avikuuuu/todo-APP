65d824c1df4cb9ca451cc5743c98ee8b6d6f57ee
avikuuuu <ku.kumar.avdesh786@gmail.com>
2023-06-12 00:55:26 +0530

    done

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_rest_api/screens/add_page.dart';
 import 'package:flutter_todo_rest_api/utils/snackbar_helper.dart';
 import 'package:flutter_todo_rest_api/todo%20services/todo_services.dart';
 import 'package:flutter_todo_rest_api/widgets/todo_card.dart';
-import 'package:http/http.dart' as http;
+
 
 class TODOListPage extends StatefulWidget {
   const TODOListPage({super.key});
@@ -18,7 +18,7 @@ class _TODOListPageState extends State<TODOListPage> {
   bool isLoading = true;
   @override
   void initState() {
-    // TODO: implement initState
+
     super.initState();
     fetchTODO();
   }
@@ -31,7 +31,7 @@ class _TODOListPageState extends State<TODOListPage> {
       ),
       body: Visibility(
         visible: isLoading,
-        replacement: Center(child: CircularProgressIndicator()),
+        replacement: const Center(child: CircularProgressIndicator()),
         child: RefreshIndicator(
           onRefresh: fetchTODO,
           child: Visibility(
@@ -44,10 +44,9 @@ class _TODOListPageState extends State<TODOListPage> {
             ),
             child: ListView.builder(
               itemCount: items.length,
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               itemBuilder: (context, index) {
                 final item = items[index] as Map;
-                final id = item['_id'] as String;
                 return TodoCard(index: index, item: item, navigateEdit:navigateToEditPage,delteByID: deleteById,);
               },
             ),
@@ -74,7 +73,7 @@ class _TODOListPageState extends State<TODOListPage> {
 
   Future<void> navigateToAddPage() async {
     await Navigator.push(
-        context, MaterialPageRoute(builder: (context) => AddTodoPage()));
+        context, MaterialPageRoute(builder: (context) => const AddTodoPage()));
 
     setState(() {
       isLoading = true;
@@ -87,7 +86,7 @@ class _TODOListPageState extends State<TODOListPage> {
     final isSuccess = await TodoServices.deleteById(id);
 
     if (isSuccess) {
-      successMessage(context, 'deleted successfully');
+       successMessage(context, 'deleted successfully');
       final filtered = items.where((element) => element['_id'] != id).toList();
       setState(() {
         items = filtered;
